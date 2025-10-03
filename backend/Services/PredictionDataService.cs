01ee1775691086f48977a5f8e8c96eb4c073bbbb
Evan Miller <evmill47@gmail.com>
2025-10-03 13:38:12 -0400

    Updated Read.ME for Coooper while I'm gone

--- backend/Services/PredictionDataService.cs
+++ backend/Services/PredictionDataService.cs
@@ -1,60 +1,60 @@
-using backend.Models;
+// using backend.Models;
 
-namespace backend.Services
-{
-    public interface IPredictionDataService
-    {
-        public Task<IEnumerable<PredictionData>> GetPredictionDataForTimeframe(int startYear, int EndYear);
+// namespace backend.Services
+// {
+//     public interface IPredictionDataService
+//     {
+//         public Task<IEnumerable<PredictionData>> GetPredictionDataForTimeframe(int startYear, int EndYear);
 
-        // TODO: Need prediction data per event, as this is what we will be giving our model to predict the outcome
-        public Task<PredictionData> GetPredictionDataForEventAsync(Event game);
-    }
+//         // TODO: Need prediction data per event, as this is what we will be giving our model to predict the outcome
+//         public Task<PredictionData> GetPredictionDataForEventAsync(Event game);
+//     }
 
-    public class PredictionDataService : IPredictionDataService
-    {
-        private readonly ISeasonService _seasonService;
+//     public class PredictionDataService : IPredictionDataService
+//     {
+//         private readonly ISeasonService _seasonService;
 
-        public PredictionDataService(ISeasonService seasonService)
-        {
-            _seasonService = seasonService;
-        }
+//         public PredictionDataService(ISeasonService seasonService)
+//         {
+//             _seasonService = seasonService;
+//         }
 
-        public async Task<IEnumerable<PredictionData>> GetPredictionDataForTimeframe(int startYear, int EndYear)
-        {
-            var seasons = await _seasonService.GetSeasonsRangedAsync(2020, 2025);
+//         public async Task<IEnumerable<PredictionData>> GetPredictionDataForTimeframe(int startYear, int EndYear)
+//         {
+//             var seasons = await _seasonService.GetSeasonsRangedAsync(2020, 2025);
 
-            var predictionData = new List<PredictionData>();
+//             var predictionData = new List<PredictionData>();
 
-            using var semaphore = new SemaphoreSlim(5);
+//             using var semaphore = new SemaphoreSlim(5);
 
-            var tasks = seasons.Select(async season =>
-            {
-                await semaphore.WaitAsync();
-                try
-                {
-                    var seasonPredictionData = await GetPredictionDataBySeason(season);
-                    lock (predictionData)
-                    {
-                        predictionData.AddRange(seasonPredictionData);
-                    }
-                }
-                finally { semaphore.Release(); }
-            });
+//             var tasks = seasons.Select(async season =>
+//             {
+//                 await semaphore.WaitAsync();
+//                 try
+//                 {
+//                     var seasonPredictionData = await GetPredictionDataBySeason(season);
+//                     lock (predictionData)
+//                     {
+//                         predictionData.AddRange(seasonPredictionData);
+//                     }
+//                 }
+//                 finally { semaphore.Release(); }
+//             });
 
-            await Task.WhenAll(tasks);
-            return predictionData.OrderBy(pd => pd.SeasonYear);
-        }
+//             await Task.WhenAll(tasks);
+//             return predictionData.OrderBy(pd => pd.SeasonYear);
+//         }
 
 
-        // TODO: Need to get season data and turn each event into a predictionData model
-        private async Task<IEnumerable<PredictionData>> GetPredictionDataBySeason(int seasonNumber)
-        {
+//         // TODO: Need to get season data and turn each event into a predictionData model
+//         private async Task<IEnumerable<PredictionData>> GetPredictionDataBySeason(int seasonNumber)
+//         {
 
-        }
+//         }
 
-        public async Task<PredictionData> GetPredictionDataForEventAsync(Event game)
-        {
+//         public async Task<PredictionData> GetPredictionDataForEventAsync(Event game)
+//         {
             
-        }
-    }
-}
+//         }
+//     }
+// }
--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae604e5e39157d9edece476ef825e6f0befdb79a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba698decede6541d93c8e3ca05ac2b5cecc7ebb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
